aa1eaaf6f5a0674bc9e1e2a24b20b5a08fc2b43e
Joe Critchley <joecritch@gmail.com>
2018-05-28 17:28:56 +0100

    main.js cleanup

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-ready(() => {
-  window.joinForm = new JoinForm(document.querySelector('[data-behavior="JoinForm"'));
-  // const textInputEls = document.querySelectorAll('[data-behavior="TextInput"');
-  // window.textInputs = [];
-  // for (let i = 0; i < textInputEls.length; i++) {
-    // window.textInputEls.push(new TextInput(textInputEls[i]));
-  // }
-  // window.submitBtn = new SubmitBtn(document.querySelector('[data-behavior="SubmitBtn"'));
-
-  // joinForm.setProps({});
-
+document.addEventListener('DOMContentLoaded', () => {
+  // This would normally be managed separately.
+  window.joinForm = new JoinForm(
+    document.querySelector('[data-behavior="JoinForm"')
+  );
 });
